9baee9de38a07cc5eb9ec08c4eda50f90ac66eb9
mehdi <mehdirizvi03@gmail.com>
2020-08-26 14:22:01 -0400

    adding python code

---
+++ Scrape.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+page = requests.get('https://api.louisvuitton.com/api/eng-ca/catalog/filter/epy4e8?range=0-50')
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+print(soup)
+
+
