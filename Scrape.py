a74829134f10e5bb3944d15c20746130de3d22b3
mehdi <mehdirizvi03@gmail.com>
2020-08-27 15:52:32 -0400

    removed certain text from span element while achieving span element for price of item

--- Scrape.py
+++ Scrape.py
@@ -10,10 +10,9 @@ soup = BeautifulSoup(r.content, 'html.parser')
 bodies = (soup.find(id='content'))
 
 clothing = bodies.find_all("ul", "grid--full product-grid-items")
-
-for span_tag in soup.findAll('span'):
+for span_tag in soup.findAll('span', 'visually-hidden'):
     span_tag.replace_with('')
 
 print(clothing[0].find("p", 'product-title').get_text())
-
+print(clothing[0].find("span", 'grid-price-money').get_text())
 
