01f225b5dfc2df5217aff96ac015d936338f9221
mehdi <mehdirizvi03@gmail.com>
2020-08-28 15:17:44 -0400

    selium added to scroll and load entire webpage

--- Scrape.py
+++ Scrape.py
@@ -1,19 +1,39 @@
-import requests
+from selenium import webdriver
 from bs4 import BeautifulSoup
+import time
 
-url = 'https://ca.octobersveryown.com/collections/all'
 
-r = requests.get(url)
+PATH = "C:\Program Files (x86)\chromedriver.exe"
 
-soup = BeautifulSoup(r.content, 'html.parser')
+driver = webdriver.Chrome(PATH)
+
+driver.get("https://ca.octobersveryown.com/collections/all")
+
+scrolls = 11
+while True:
+    scrolls -= 1
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+    time.sleep(0.1)
+    if scrolls < 0:
+        break
+
+html = driver.page_source
+
+soup = BeautifulSoup(html, 'html.parser')
 
 bodies = (soup.find(id='content'))
 
-clothing = bodies.find_all("ul", "grid--full product-grid-items")
-for span_tag in soup.findAll('span', 'visually-hidden'):
+clothing = bodies.find_all(class_='grid--full product-grid-items')
+for span_tag in soup.findAll(class_='visually-hidden'):
     span_tag.replace_with('')
 
 print(clothing[0].find('img')['src'])
 print(clothing[0].find(class_='product-title').get_text())
 print(clothing[0].find(class_='grid-price-money').get_text())
+
+
+
+time.sleep(8)
+
+driver.quit()
 
