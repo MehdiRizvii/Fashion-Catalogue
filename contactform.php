b79e357e385bf145bc4b7dfbfb17fffa9721d7d8
mehdi <mehdirizvi03@gmail.com>
2020-09-08 18:52:01 -0400

    finished php

--- contactform.php
+++ contactform.php
@@ -6,7 +6,7 @@ if (isset($_POST['submit'])){
     $mailFrom = $_POST['mail'];
     $message = $_POST['message'];
 
-    $mailTo = "luxgoodstome@gmail.com";
+    $mailTo = "contact@lauxauras.com";
 $headers = "From: ".$mailFrom;
 $txt = "You have received an e-mail from".$name.".\n\n".$message;
     mail($mailTo, $subject, $txt, $headers);
