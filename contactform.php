51b71e28cec5bbccdcac11e483b8cdf7e019d77e
mehdi <mehdirizvi03@gmail.com>
2020-09-08 16:54:19 -0400

    added php form

--- contactform.php
+++ contactform.php
@@ -6,4 +6,9 @@ if (isset($_POST['submit'])){
     $mailFrom = $_POST['mail'];
     $message = $_POST['message'];
 
+    $mailTo = "luxgoodstome@gmail.com";
+$headers = "From: ".$mailFrom;
+$txt = "You have received an e-mail from".$name.".\n\n".$message;
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.php?mailsend");
 }
